2d6d8b29a3e6b92fed2f76cc46bb107ed322f400
manjit <manjit.kaur@stud.hs-hannover.de>
2017-04-23 16:15:35 +0200

    database

--- app/src/main/java/com/healthservices/mha/momole/database/MomoleDAO.java
+++ app/src/main/java/com/healthservices/mha/momole/database/MomoleDAO.java
@@ -28,19 +28,19 @@ public class MomoleDAO {
     public static final String TBL_N_TSTMP = "tspmp";
     public static final String TBL_N_AMOUNT = "amount";
 
-    public static final String CREATE_TBL = "CREATE TBL " + TBL_LM + "("
+    public static final String CREATE_TBL_LM = "CREATE TBL " + TBL_LM + "("
             + TBL_LM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + TBL_LM_TSTMP + " INTEGER NOT NULL, "
             + TBL_LM_AMOUNT + " REAL NOT NULL, "
             + TBL_LM_CATEGORY + " TEXT";
 
-    public static final String CREATE_TBL = "CREATE TBL " + TBL_B + "("
+    public static final String CREATE_TBL_B = "CREATE TBL " + TBL_B + "("
             + TBL_B_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + TBL_B_TSTMP + " INTEGER NOT NULL, "
             + TBL_B_AMOUNT + " REAL NOT NULL, "
             + TBL_B_CATEGORY + " TEXT";
 
-    public static final String CREATE_TBL = "CREATE TBL " + TBL_N + "("
+    public static final String CREATE_TBL_N = "CREATE TBL " + TBL_N + "("
             + TBL_N_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + TBL_N_TSTMP + " INTEGER NOT NULL, "
             + TBL_N_AMOUNT + " REAL NOT NULL";
