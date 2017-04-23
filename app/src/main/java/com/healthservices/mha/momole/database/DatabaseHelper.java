51dcca0d5ac715f66afd2211b3aa54b368b7dd1d
manjit <manjit.kaur@stud.hs-hannover.de>
2017-04-23 15:51:52 +0200

    database

--- app/src/main/java/com/healthservices/mha/momole/database/DatabaseHelper.java
+++ app/src/main/java/com/healthservices/mha/momole/database/DatabaseHelper.java
@@ -8,9 +8,30 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by manji on 23.04.2017.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "momole.db";
+    private static final int DATABASE_VERSION = 1;
+    private static DatabaseHelper instance;
 
+    private DatabaseHelper(Context context, SQLiteDatabase.CursorFactory factory){
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+    public static DatabaseHelper getInstance (Context context){
+        if (instance == null){
+            instance = new DatabaseHelper(context, null);
+        }
+        return instance;
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        MomoleDAO.getInstance(null).onCreate(db);
+    }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        MomoleDAO.getInstance(null).onUpgrade(db, oldVersion, newVersion);
+    }
 }
+
---
+++ app/src/main/java/com/healthservices/mha/momole/database/MomoleDAO.java
@@ -0,0 +1,8 @@
+package com.healthservices.mha.momole.database;
+
+/**
+ * Created by manji on 23.04.2017.
+ */
+
+public class MomoleDAO {
+}
