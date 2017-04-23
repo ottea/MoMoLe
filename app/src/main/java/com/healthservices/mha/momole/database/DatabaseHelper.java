6396ea986d8f77351b6813fbcb385c6e90570ff7
manjit <manjit.kaur@stud.hs-hannover.de>
2017-04-23 15:42:57 +0200

    database

--- app/src/main/java/com/healthservices/mha/momole/database/DatabaseHelper.java
+++ app/src/main/java/com/healthservices/mha/momole/database/DatabaseHelper.java
@@ -1,8 +1,16 @@
 package com.healthservices.mha.momole.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by manji on 23.04.2017.
  */
 
 public class DatabaseHelper {
+
+
+
+
 }
